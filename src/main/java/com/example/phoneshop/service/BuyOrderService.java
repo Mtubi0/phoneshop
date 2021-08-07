1d87566701d8d26929468b6ca26dcafc44f14d3b
Mat <mtubio@cash-online.com.ar>
2021-08-07 15:00:21 -0300

    first commit

---
+++ src/main/java/com/example/phoneshop/service/BuyOrderService.java
@@ -0,0 +1,24 @@
+package com.example.phoneshop.service;
+
+import java.util.List;
+
+import com.example.phoneshop.model.BuyOrder;
+import com.example.phoneshop.repository.BuyOrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuyOrderService {
+
+    @Autowired
+    private BuyOrderRepository buyOrderRepository;
+
+    public List<BuyOrder> getAllBuyOrders() {
+        return buyOrderRepository.findAll();
+    }
+
+    public void createBuyOrder(BuyOrder someBuyOrder) {
+        buyOrderRepository.save(someBuyOrder);
+    }
+}
